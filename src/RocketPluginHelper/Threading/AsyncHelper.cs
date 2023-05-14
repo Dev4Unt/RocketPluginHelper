c903f2efe0aeb5482cee38e3f877f8676d5068d3
sunnamed434 <cetnikletsplay@gmail.com>
2023-05-14 10:33:14 +0300

    Init project

---
+++ src/RocketPluginHelper/Threading/AsyncHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace RocketPluginHelper.Threading;
+
+public static class AsyncHelper
+{
+    public static void Schedule(string name, Func<Task> task, Action<Exception>? exceptionHandler = null)
+    {
+        Task.Run(async () =>
+        {
+            try
+            {
+                await task();
+            }
+            catch (Exception e)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler(e);
+                }
+                else
+                {
+                    RocketLogger.LogException(e, $"Exception occured in task \"{name}\"");
+                }
+            }
+        });
+    }
+}
---
+++ src/RocketPluginHelper/Threading/AsyncHelperEx.cs
@@ -0,0 +1,29 @@
+namespace RocketPluginHelper.Threading;
+
+public static class AsyncHelperEx
+{
+    public static void Schedule(string name, Func<Task> task, Action<Exception>? exceptionHandler = null)
+    {
+        Task.Run(async () =>
+        {
+            try
+            {
+                await task();
+            }
+            catch (Exception ex)
+            {
+                if (ex is not OperationCanceledException)
+                {
+                    if (exceptionHandler == null)
+                    {
+                        RocketLogger.LogException(ex, $"Exception occured in task {name}.");
+                    }
+                    else
+                    {
+                        exceptionHandler(ex);
+                    }
+                }
+            }
+        });
+    }
+}
