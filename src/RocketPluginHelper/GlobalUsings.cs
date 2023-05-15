89e1d8aa42eb2e109b9a9142702edae1660dedea
sunnamed434 <cetnikletsplay@gmail.com>
2023-05-15 10:15:24 +0300

    Add OpenMod integration

--- src/RocketPluginHelper/GlobalUsings.cs
+++ src/RocketPluginHelper/GlobalUsings.cs
@@ -1,9 +1,12 @@
 ﻿global using System;
 global using System.Diagnostics.CodeAnalysis;
+global using System.IO;
+global using System.Linq;
 global using System.Reflection;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using Cysharp.Threading.Tasks;
 global using JetBrains.Annotations;
+global using SDG.Unturned;
 global using UnityEngine.LowLevel;
 global using RocketLogger = Rocket.Core.Logging.Logger;
---
+++ src/RocketPluginHelper/Integrations/OpenModIntegration.cs
@@ -0,0 +1,29 @@
+﻿namespace RocketPluginHelper.Integrations;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class OpenModIntegration
+{
+    public static bool IsOpenModInstalled()
+    {
+        const string modulesDirectoryName = "Modules";
+        var modulesDirectory = Path.Combine(ReadWrite.PATH, modulesDirectoryName);
+        const string rocketModuleFile = "OpenMod.Unturned.module";
+        return Directory
+            .GetFiles(modulesDirectory, rocketModuleFile, SearchOption.AllDirectories)
+            .Any();
+    }
+    public static bool IsOpenModUnturnedLoaded(out Assembly? assembly)
+    {
+        assembly = AppDomain.CurrentDomain
+            .GetAssemblies()
+            .LastOrDefault(IsOpenModUnturnedAssembly);
+        return assembly != null;
+    }
+    private static bool IsOpenModUnturnedAssembly(Assembly assembly)
+    {
+        const string openmodUnturnedAssemblyName = "OpenMod.Unturned";
+        return assembly
+            .GetName().Name
+            .Equals(openmodUnturnedAssemblyName);
+    }
+}
