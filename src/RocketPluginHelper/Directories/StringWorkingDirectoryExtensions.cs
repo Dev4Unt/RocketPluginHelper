a2ec4c3ef9827f912fe019a19f0f2c072fa506ed
sunnamed434 <cetnikletsplay@gmail.com>
2023-05-19 20:57:07 +0300

    Add StringWorkingDirectoryExtensions

---
+++ src/RocketPluginHelper/Directories/StringWorkingDirectoryExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace RocketPluginHelper.Directories;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class StringWorkingDirectoryExtensions
+{
+    private const string WorkingDirectoryMarker = "{WorkingDirectory}";
+
+    public static string ReplaceWorkingDirectory(this string source, string to)
+    {
+        return source.Replace(WorkingDirectoryMarker, to);
+    }
+}
