978ee075827246d90d9a07196694af59a346fd24
sunnamed434 <cetnikletsplay@gmail.com>
2023-06-14 21:40:22 +0300

    Add Schedule usage Context alternative to name

--- src/RocketPluginHelper/Threading/AsyncHelper.cs
+++ src/RocketPluginHelper/Threading/AsyncHelper.cs
@@ -3,6 +3,10 @@
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public static class AsyncHelper
 {
+    public static void Schedule<TContext>(Func<Task> task, Action<Exception>? exceptionHandler = null)
+    {
+        Schedule(typeof(TContext).FullName, task, exceptionHandler);
+    }
     public static void Schedule(string name, Func<Task> task, Action<Exception>? exceptionHandler = null)
     {
         Task.Run(async () =>
