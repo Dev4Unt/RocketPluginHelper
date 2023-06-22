a1ffaf755ece21460699c6836cb8511b32bc9285
sunnamed434 <cetnikletsplay@gmail.com>
2023-06-22 13:32:24 +0300

    Update CancellationTokenExtensions.cs

--- src/RocketPluginHelper/Threading/CancellationTokenExtensions.cs
+++ src/RocketPluginHelper/Threading/CancellationTokenExtensions.cs
@@ -3,7 +3,6 @@
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public static class CancellationTokenExtensions
 {
-    [SuppressMessage("ReSharper", "InvertIf")]
     public static void CancelAndDispose(this CancellationTokenSource source)
     {
         using var _ = source;
