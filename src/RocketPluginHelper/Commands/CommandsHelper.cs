e9f8bb18553592120d2baa555fe658beca15e1f1
sunnamed434 <cetnikletsplay@gmail.com>
2023-07-03 21:20:14 +0300

    Add CSteamIdHelper and RocketModIntegration

---
+++ src/RocketPluginHelper/Commands/CommandsHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace RocketPluginHelper.Commands;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+[SuppressMessage("ReSharper", "InvertIf")]
+public static class CommandsHelper
+{
+    public static bool TryExecuteCommand(string command)
+    {
+        if (RocketModIntegration.IsRocketModUnturnedLoaded(out _))
+        {
+            if (R.Commands.Execute(new ConsolePlayer(), command))
+            {
+                return true;
+            }
+        }
+        return Commander.execute(CSteamID.Nil, command);
+    }
+}
---
+++ src/RocketPluginHelper/Helpers/CSteamIdHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace RocketPluginHelper.Helpers;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class CSteamIdHelper
+{
+    public static CSteamID GetSteamId(this IRocketPlayer rocketPlayer)
+    {
+        if (rocketPlayer is UnturnedPlayer unturnedPlayer)
+        {
+            return unturnedPlayer.CSteamID;
+        }
+        return Provider.server;
+    }
+}
